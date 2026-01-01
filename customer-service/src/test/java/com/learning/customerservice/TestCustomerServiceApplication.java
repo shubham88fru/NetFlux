0a559d6eb89c97bb8dab5c7342d16ecd1d7106ae
shubham88fru <shubham88fru@yahoo.com>
2026-01-01 15:08:41 -0600

    customer service initial commit

---
+++ customer-service/src/test/java/com/learning/customerservice/TestCustomerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.learning.customerservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CustomerServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
